f318a669ef13522bf2de6ac472e9ddf664f6a8a0
ally-ahmed <norbixin@live.com>
2018-04-03 19:25:19 +0300

    :construction: WIP Added logic to show active player

--- js/app.js
+++ js/app.js
@@ -1,8 +1,8 @@
 
-let scores, roundScores, activePlayer;
+let scores, roundScore, activePlayer;
 
 scores = [0,0]; // scores for P1 and P2
-roundScores = 0; // current score, we only need one per round
+roundScore = 0; // current score, we only need one per round
 
 /** 
  * 0 is player1 and 1 is player2
@@ -15,11 +15,21 @@ document.getElementById('score-1').textContent = '0';
 document.getElementById('current-0').textContent = '0';
 document.getElementById('current-1').textContent = '0';
 
-// document.querySelector(`#current-${activePlayer}`).textContent = dice;
-
 document.querySelector('.btn-roll').addEventListener('click',function () {  
     let dice = Math.floor(Math.random() * 6)+ 1;
     let diceDom = document.querySelector('.dice-img');
-    diceDom.src = `images/die-${dice}.svg`
-});
+    diceDom.src = `images/die-${dice}.svg`;
+    if(dice > 1){
+        roundScore += dice;
+        document.querySelector(`#current-${activePlayer}`).textContent = roundScore;
+    }else {
+        roundScore = 0;
+        document.querySelector(`#current-${activePlayer}`).textContent = roundScore;
+        document.getElementById(`player-${activePlayer}`).classList.remove('border-primary');
+        document.getElementById(`player-${activePlayer}`).classList.add('border-secondary');
 
+        (activePlayer === 0) ? activePlayer = 1 : activePlayer = 0;
+        document.getElementById(`player-${activePlayer}`).classList.remove('border-secondary');
+        document.getElementById(`player-${activePlayer}`).classList.add('border-primary')
+    }
+});
